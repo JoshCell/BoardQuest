a04859de13077701a07ed0b451967feca3366835
JoshCell <cellsius_bdn@hotmail.com>
2017-05-24 13:10:14 +0200

    Update casillas.js

--- models/casillas.js
+++ models/casillas.js
@@ -13,7 +13,7 @@ var cas = mongoose.model("casillas", casillas_schema);
 var nueva = new cas({ID_num:001, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
 nueva.save(function(err){ console.log(err); });
 
-//CASILLAS EVENTO 
+//CASILLAS EVENTO
 
 
 
@@ -23,3 +23,357 @@ nueva.save(function(err){ console.log(err); });
 
 
 // CASILLAS BOSS
+
+
+// CASILLAS POR DETERMINAR
+
+var nueva = new cas({ID_num:002, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:003, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:004, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:005, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:006, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:007, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:008, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:009, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:010, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:011, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:012, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:013, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:014, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:015, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:016, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:017, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:018, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:019, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:020, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:021, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:022, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:023, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:024, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:025, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:026, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:027, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:028, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:029, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:030, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:031, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:032, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:033, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:034, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:035, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:036, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:037, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:038, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:039, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:040, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:041, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:042, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:043, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:044, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:045, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:046, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:047, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:048, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:049, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:050, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:051, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:052, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:053, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:054, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:055, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:056, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:057, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:058, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:059, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:060, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:061, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:062, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:063, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:064, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:065, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:066, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:067, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:068, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:069, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:070, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:071, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:072, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:073, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:074, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:075, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:076, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:077, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:078, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:079, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:080, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:081, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:082, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:083, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:084, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:085, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:086, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:087, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:088, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:089, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:090, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:091, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:092, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:093, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:094, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:095, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:096, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:097, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:098, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:099, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:100, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:101, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:102, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:103, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:104, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:105, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:106, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:107, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:108, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:109, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:110, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:111, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:112, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:113, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:114, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:115, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:116, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:117, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
+
+var nueva = new cas({ID_num:118, Tipo:"", PosX:0, PosY:0, Evento:"", url1:"", url2:""});
+nueva.save(function(err){ console.log(err); });
